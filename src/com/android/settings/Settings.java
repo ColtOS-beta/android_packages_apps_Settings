ziasam/platform_packages_apps_Settings

Add ColtEnigma in to the Settings Dashboard
Change-Id: I8543b8af6f508e2ead192c7ee1fdc9603cc5b06d
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/Settings.java
@@ -234,6 +234,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class ColtSettingsActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,8 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.colt.settings.ColtSettings;
+
 public class SettingsGateway {
 
     /**
@@ -307,7 +309,8 @@ public class SettingsGateway {
             GestureNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
-            MediaControlsSettings.class.getName()
+            MediaControlsSettings.class.getName(),
+            ColtSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -347,5 +350,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.ColtSettingsActivity.class.getName(),
     };
-}
+            }

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -46,6 +46,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.colt.settings.ColtSettings;
+
 import java.util.Map;
 
 /**
@@ -119,6 +121,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(ColtSettings.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 

ColtOS-beta/android_packages_apps_Settings

src/com/android/settings/Settings.java
@@ -500,6 +500,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class ColtEnigmaActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -195,6 +195,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.colt.enigma.ColtEnigma;
+
 public class SettingsGateway {
 
     /**
@@ -377,7 +379,8 @@ public class SettingsGateway {
             LongBackgroundTasksDetails.class.getName(),
             RegionalPreferencesEntriesFragment.class.getName(),
             BatteryInfoFragment.class.getName(),
-            UserAspectRatioDetails.class.getName()
+            UserAspectRatioDetails.class.getName(),
+            ColtEnigma.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -426,5 +429,6 @@ public class SettingsGateway {
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.FirmwareVersionActivity.class.getName(),
             Settings.WallpaperSettingsActivity.class.getName(),
+            Settings.ColtEnigmaActivity.class.getName(),
     };
-}
+            }

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -51,6 +51,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.colt.enigma.ColtEnigma;
+
 import java.util.Map;
 
 /**
@@ -135,6 +137,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_SPECIAL_APP_ACCESS);
         PARENT_TO_CATEGORY_KEY_MAP.put(MoreSecurityPrivacyFragment.class.getName(),
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(ColtEnigma.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 